a8931a02b74c0f93962cccdd63c44a0e8e20bfc1
Trevor Bekolay <tbekolay@gmail.com>
2015-04-29 15:19:41 -0400

    Release version 0.1.0

--- nengo_distilled/version.py
+++ nengo_distilled/version.py
@@ -1,6 +1,6 @@
 version_info = (0, 1, 0)  # (major, minor, patch)
 rc = None
-dev = True
+dev = False
 
 version = "{0}{1}{2}".format('.'.join(str(v) for v in version_info),
                              '-rc{0:d}'.format(rc) if rc is not None else '',
